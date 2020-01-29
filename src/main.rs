df9222c72673eeffb7e11a710527203374ee870c
samparsky <8148384+samparsky@users.noreply.github.com>
2020-01-29 17:03:38 +0100

    add: generate p2pkh address fn

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use clap::{Arg, App};
+use secp256k1::{Secp256k1, SecretKey, PublicKey};
+use sha2::{Sha256};
+use ripemd160::{Ripemd160, Digest};
+use base58check::ToBase58Check;
 
 fn main() {
     let app = App::new("Bitcoin Address Generator")
@@ -27,4 +31,40 @@ fn main() {
                     .long("key_pair")
                     .help("Outputs Random Secp256k1 public and compressed private key"))
                 .get_matches();
+}
+
+fn p2pkh(private_key: &str) {
+    
+    title("Generating P2PKH Address");
+
+    let private_key = hex::decode(private_key).expect("Invalid private key supplied");
+    let secp = Secp256k1::new();
+    let secret_key = SecretKey::from_slice(&private_key).expect("32 bytes, within curve order");
+    let public_key = PublicKey::from_secret_key(&secp, &secret_key).to_string();
+
+    // Sha256
+    let mut hasher = Sha256::new();
+    hasher.input(&hex::decode(&public_key).expect("should decode"));
+    let result = hasher.result();
+
+    // Ripemd160
+    let mut ripemd_hash = Ripemd160::new();
+    ripemd_hash.input(&result);
+    let ripemd_result = ripemd_hash.result().as_slice().to_owned();
+
+    // Base58Check encoding
+    let address = ripemd_result.to_base58check(0);
+
+    println!("P2PKH Address: \n{}", address);
+
+    end();
+}
+
+
+fn title(title: &str) {
+    println!("================= {} =================", title);   
+}
+
+fn end() {
+    println!("======================================");
 }
